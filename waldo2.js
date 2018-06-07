e6ab296fa6ff19832fc9fc752358b78b66f13471
Wisara8 <srandall845@gmail.com>
2018-06-07 18:51:15 +0000

    refrector to use forEach

---
+++ waldo2.js
@@ -0,0 +1,16 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function (name) {
+    if (name === "Waldo") {
+      found(arr.indexOf(name));
+    }   // execute callback
+    });
+}
+
+function actionWhenFound(index) {
+  console.log("Found him at index " + index + "!");
+}
+
+
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
